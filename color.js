8294db539ad6ec1a9431c08d69a7de50c4d5c38a
VincentVazquez86 <7vvazquez@stpatrick.org>
2016-11-09 11:27:35 -0600

    Add files via upload

---
+++ color.js
@@ -0,0 +1,37 @@
+//Declare the Color Object with our new keyword below here.
+object.color
+
+/*
+** rgb is an array that hold the color values.
+** first is red -> R, then green -> G, and blue -> B
+** Example
+*/
+
+Color.isColor = function(rgb){
+  //each color must be between 0 and 255. Also can be equal.
+
+  let redworks = (rgb[0] >= 0) && (rgb[0] <= 255);
+  let greenworks
+  let blueworks
+
+  if(redworks && greenworks && blueworks){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+Color.redIntensity = function(rgb){
+
+};
+//greenIntensity
+greenIntensity=G
+
+//blueIntensity
+
+
+//brightness
+
+
+//complement
